7b481f9381cb07df8b271c38652b0805577a1ed6
ProphetLamb <prophet.lamb@gmail.com>
2026-02-07 22:58:12 +0100

    Lower duability threshold to 30

--- weight_specific_durability.sql
+++ weight_specific_durability.sql
@@ -1,3 +1,3 @@
 SELECT (p.vehicle_durability * (1.0 + (COALESCE(p.feature_bullet, 0) / 100.0))  / (1 - (COALESCE(p.feature_passthru, 0)) / 100.0) /  p.mass) as weight_specific_durability, p.* FROM parts AS p
-WHERE p.durability >= 40
+WHERE COALESCE(p.vehicle_durability,0) >= 30
 ORDER BY (p.vehicle_durability * (1.0 + (COALESCE(p.feature_bullet, 0) / 100.0))  / (1 - (COALESCE(p.feature_passthru, 0)) / 100.0) /  p.mass) DESC,  p.name
